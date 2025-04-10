fbb8d445af74188bf1132ac6391b7e5c22473784
Alyssa Ngo <amaengo@protonmail.com>
2025-04-10 10:48:42 +1000

    Added some UI elements for the blog profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-
-import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
@@ -10,42 +7,32 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+      <div className="bg-blue-600 flex items-center justify-between p-10 text-white">
+        <h1 className="text-3xl font-bold">T3 Trends</h1>
+        <div className="flex justify-between gap-6 text-2xl mr-18">
+          <div>Dashboard</div>
+          <div>Logout</div>
+        </div>
+      </div>
+      <main className="flex min-h-screen flex-col bg-gray-200 text-black">
+        <div className="container flex flex-col items-center justify-center gap-12 mx-20 py-16">
+          <div className="bg-white rounded-lg w-200 h-auto space-y-4 outline outline-gray-300 py-8">
+            <div className="ml-10">
+              <div className="text-3xl font-bold pb-6">User Activity</div>
+              <div className="flex gap-4">
+                <div className="bg-blue-600 rounded-md py-4 w-35 h-15">
+                  <p className="text-white text-center text-lg font-bold">Posts</p>
+                </div>
+                <div className="bg-gray-100 rounded-md py-4 w-35 h-15 outline-2 outline-gray-300">
+                  <p className="text-black text-center text-lg">Comments</p>
+                </div>
+                <div className="bg-gray-100 rounded-md py-4 w-35 h-15 outline-2 outline-gray-300">
+                  <p className="text-black text-center text-lg">Likes</p>
+                </div>
               </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+              
+            </div>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
         </div>
       </main>
     </HydrateClient>
